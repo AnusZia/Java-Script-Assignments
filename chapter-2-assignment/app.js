53543b472f6f63b98c5399a473b1a7e21cf18a4d
AnusZia <154527377+AnusZia@users.noreply.github.com>
2024-03-04 00:39:09 +0500

    Add files via upload

---
+++ chapter-2-assignment/app.js
@@ -0,0 +1,52 @@
+// // Q1:ANSWER
+// var username;
+
+
+// // Q2:ANSWER
+// var myName = "Anus Khan";
+
+
+// // Q3:ANSWER
+// var message = "Hello World";
+// alert(message);
+
+
+
+
+// // Q4:ANSWER
+// var studentName = "Johne Doe";
+// var age = "15 years old";
+// var certification = "Certified Mobile Application Development";
+
+
+// alert( studentName);
+// alert( age );
+// alert(  certification);
+
+
+// // Q5:ANSWER
+// var alertMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(alertMessage);
+
+// // Q6:ANSWER
+// var email = "anusk8360@gmail.com";
+// alert("My email address is " + email);
+
+
+// // Q7:ANSWER
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying  to learn from the Book " + book);
+
+
+// Q8:ANSWER
+// var message="Yah! I can write HTML content through JavaScript"
+// document.write( message);
+
+
+// Q8:ANSWER
+var Message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(Message);
+document.write(Message);
+
+
+
