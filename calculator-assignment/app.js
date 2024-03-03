cefd91eb8f0b408cafd34b324885649a42def07a
AnusZia <154527377+AnusZia@users.noreply.github.com>
2024-03-04 00:40:10 +0500

    Add files via upload

---
+++ calculator-assignment/app.js
@@ -0,0 +1,28 @@
+
+
+
+let num1 = +prompt("Enter Number 1");
+let num2 = +prompt("Enter Number 2");
+
+
+ var message="......This Is My Calculator....... \n"
+ document.write(message)
+
+ var messagee="<br><br>......Here Is Your Calculation 👇....... \n"
+ document.write(messagee)
+
+let sum = num1 + num2;
+document.write(" <br/><br>Sum of two numbers = ", sum +"<br/>");
+
+let sub = num1 - num2;
+document.write("Subtraction of two numbers = ", sub +"<br/>");
+
+
+let mul = num1 * num2;
+document.write("Multiplication of two numbers = ", mul +"<br/>");
+
+let div = num1 / num2;
+document.write("Division of two numbers = ", div +"<br/>");
+
+let reminder = num1 | num2;
+document.write("Reminder of two numbers = ", reminder +"<br/>");
