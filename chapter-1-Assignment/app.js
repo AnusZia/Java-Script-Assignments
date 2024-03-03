c56866b4cc7e899de81cd010984a374be3c90865
AnusZia <154527377+AnusZia@users.noreply.github.com>
2024-03-03 23:41:01 +0500

    Add files via upload

---
+++ chapter-1-Assignment/app.js
@@ -0,0 +1,42 @@
+// Q1:Answer
+
+// alert("Welcome to Our Website! We're glad you're here.");
+
+
+
+// Q2:Answer 
+
+// alert("Error! Please enter a valid password")
+
+
+
+// Q3:Answer
+
+// alert("Welcome to JS Land... \nHappy Coding")
+
+
+
+
+// Q4:Answer
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs ")
+
+
+
+
+// Q5 :Answer
+
+var message = "Hello...I can run JS through my web brower's console";
+
+console.log(message);
+
+alert(message);
+
+
+
+
+
+
+
+ 
