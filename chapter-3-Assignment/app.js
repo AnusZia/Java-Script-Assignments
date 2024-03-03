4b7bb75df13cbd1717a5bfd9034d0ef6111fda8c
AnusZia <154527377+AnusZia@users.noreply.github.com>
2024-03-04 00:39:36 +0500

    Add files via upload

---
+++ chapter-3-Assignment/app.js
@@ -0,0 +1,30 @@
+// Q1:ANSWER
+// var age = 18; 
+// alert("I am"  +age+ " years old");
+
+
+// Q2:ANSWER
+// var birthYear = 2004;
+// document.write("My birth year is " + birthYear + "<br/>");
+// document.write("Data type of my declared variable is number" );
+
+
+
+// Q3:ANSWER
+// var visits=14;
+// alert("You have visited this site "+visits+" times")
+
+
+
+
+
+
+
+// Q4:ANSWER
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity  +" " +  productTitle + "(s) on XYZ Clothing store.");
+
+
