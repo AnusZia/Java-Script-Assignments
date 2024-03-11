a0d1813dff83e7e4e06093a80cc6a1e6413e7dce
AnusZia <154527377+AnusZia@users.noreply.github.com>
2024-03-11 23:55:01 +0500

    Add files via upload

---
+++ chapter-4-assignment/app.js
@@ -0,0 +1,27 @@
+// Q1:answer
+// let variable1, variable2, variable3;
+
+
+// Q2:Answer
+// Legal variable names:
+// 1. legalVariableName
+// 2. myVar
+// 3. _variable
+// 4. camelCaseVariable
+// 5. $moneyAmount
+
+// Illegal variable names:
+// 1. 123variable (starts with a number)
+// 2. my-variable (contains a hyphen)
+// 3. var (reserved keyword)
+// 4. my variable (contains a space)
+// 5. %illegalVariable (contains special characters other than underscore or dollar sign)
+
+// Q3:Answer
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain numbers,$ or _. for example:$my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter,$ or _.for example : $name,_name or name</p>");
+document.write("<p>Variable names are case-sensitive</p>");
+document.write("<p>Variable names should not be JS keywords</p>");
+
+
